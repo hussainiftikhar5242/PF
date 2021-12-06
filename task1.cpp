3813396bd8ce0046869d58ec57910da2990003d3
hussainiftikhar5242 <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-06 22:04:23 +0500

    THIS IS MY 3RD_WEEK_TASK_1 IN WHICH I DISPLAY THE * in 5 lines

--- task1.cpp
+++ task1.cpp
@@ -1,15 +1,6 @@
 #include<iostream>
 using namespace std;
-int main()
+main()
 {
-	float distance ;
-	float time;
-	cout<<"enter distance in KM:";
-	cin>> distance;
-	cout<<"enter time in hours:";
-	cin>>time;
-	float speed;
-	speed=distance/time;
-	cout<<"speed is "<< speed;
+	cout<<"*"<<endl<<"**"<<endl<<"***"<<endl<<"****"<<endl<<"*****"<<endl;
 }
-	
