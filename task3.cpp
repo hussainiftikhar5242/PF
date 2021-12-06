3715a560ed5b0b9dab924a9e3ee8bc6849b8434d
hussainiftikhar5242 <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-06 22:10:52 +0500

    My 3rd task in which I find the area.

---
+++ task3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int length,width,area;
+	cout<<"enter length in feet:";
+	cin>> length;
+	cout<<"enter width in feet:";
+	cin>> width;
+	area=length*width;
+	cout<<"e\area is :"<<area;
+}
