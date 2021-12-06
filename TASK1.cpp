b07316b82f5ff2f0f8bd23670bbfe5ef73351585
hussainiftikhar5242 <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-06 21:56:05 +0500

    WEEK_3_TASK_1
    this is my 3rd week 1st task in which i display the * in 5 line

---
+++ TASK1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1,num2,num3;
+	cout<<"enter a number:";
+	cin>> num1;
+	cout<<"enter 2nd number";
+	cin>> num2;
+	num3=num1+num2;
+	cout<< "sum is equal to"<<num3;
+}
+
