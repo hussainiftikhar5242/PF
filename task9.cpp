45cd4970f840d124cd713983df15f3c402730250
hussainiftikhar5242 <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-06 22:26:02 +0500

    I have done this homework in which i take 4 number from user and then find the sum of these numbers.

---
+++ task9.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int num1;
+	cout<<"enter four digits:";
+	cin>> num1;
+	int a=num1%10;
+	int b=num1/10;
+	b=b%10;
+	int c=num1/100;
+	c=c%10;
+	int d=num1/1000;
+	d=d%10;
+	int sum=a+b+c+d;
+	cout<<"total sum :"<<sum;
+	
+	
+
+}
+	
