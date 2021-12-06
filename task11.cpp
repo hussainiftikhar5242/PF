bb79aaaf7e4740cfbed07cd8578dd9f2b69ba199
hussainiftikhar5242 <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-06 22:55:16 +0500

    I have done this homework in which I take 15 numbers from user then add 1st five then multiply 2nd five then sub last five and then add the 1at and 2nd result and then substract form 3rd result.

---
+++ task11.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,n14,n15;
+	cout<<"enter 1st value:";
+	cin>> n1;
+	
+	cout<<"enter 2nd value:";
+	cin>> n2;
+	
+	cout<<"enter 3rd value:";
+	cin>> n3;
+	
+	cout<<"enter 4th value:";
+	cin>> n4;
+	
+	cout<<"enter 5th value:";
+	cin>> n5;
+	
+	cout<<"enter 6th value:";
+	cin>> n6;
+	
+	cout<<"enter 7th value:";
+	cin>> n7;
+	
+	cout<<"enter 8th value:";
+	cin>> n8;
+	
+	cout<<"enter 9th value:";
+	cin>> n9;
+	
+	cout<<"enter 10th value:";
+	cin>> n10;
+	
+	cout<<"enter 11th value:";
+	cin>> n11;
+	
+	cout<<"enter 12th value:";
+	cin>> n12;
+	
+	cout<<"enter 13th value:";
+	cin>> n13;
+	
+	cout<<"enter 14th value:";
+	cin>> n14;
+	
+	cout<<"enter 15th value:";
+	cin>> n15;
+	
+	int sum=n1+n2+n3+n4+n5;
+	int mul=n6*n7*n8*n9*n10;
+	int sub=n11-n12-n13-n14-n15;
+	int total_sum=sum+mul;
+	int total_sub=total_sum-sub;
+	cout<< total_sub;
+
+
+
+
+
+
+}
